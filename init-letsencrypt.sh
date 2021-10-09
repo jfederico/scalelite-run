b9e9a7c32567bdabf9b14eec1bf6da48c8a1fd51
Andrej Vitez <andrejvitez@gmail.com>
2021-10-09 22:51:45 +0200

    Fix invalid boolean variable comparisons

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -47,17 +47,16 @@ email="$LETSENCRYPT_EMAIL" # Adding a valid address is strongly recommended
 staging=${LETSENCRYPT_STAGING:-0} # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  if [ "$replaceExisting" -ne 0 ] && [ "$interactive" -eq 1 ]; then
+  if [ "$replaceExisting" -eq 0 ] && [ "$interactive" -eq 1 ]; then
     read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
     if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
       exit
     fi
-  elif [ -z "$interactive" ]; then
+  elif [ "$interactive" -eq 0 ]; then
     echo "Certificates already exist."
     exit
   fi
 fi
-
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
