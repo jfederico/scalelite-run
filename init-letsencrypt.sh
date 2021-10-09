f6618c04fc5284fae5f58f5085c7f8789ad2cfcf
Andrej Vitez <andrejvitez@gmail.com>
2021-10-09 23:03:09 +0200

    Refactor user confirmation prompt logic

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -46,16 +46,16 @@ data_path="./data/certbot"
 email="$LETSENCRYPT_EMAIL" # Adding a valid address is strongly recommended
 staging=${LETSENCRYPT_STAGING:-0} # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if [ -d "$data_path" ]; then
-  if [ "$replaceExisting" -eq 0 ] && [ "$interactive" -eq 1 ]; then
+if [ -d "$data_path" ] && [ "$replaceExisting" -eq 0 ]; then
+    if [ "$interactive" -eq 0 ]; then
+      echo "Certificates already exist."
+      exit
+    fi
+
     read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
     if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
       exit
     fi
-  elif [ "$interactive" -eq 0 ]; then
-    echo "Certificates already exist."
-    exit
-  fi
 fi
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
