0c775b44007d9035c3d67cb0e1513a54e1c6ff79
jfederico <jesus@123it.ca>
2022-10-17 14:31:59 -0400

    some updates for simplifying the dev script and added documentation for dev

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -96,7 +96,6 @@ docker-compose run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
 echo
 
-
 echo "### Requesting Let's Encrypt certificate for $domains ..."
 #Join $domains to -d args
 domain_args=""
